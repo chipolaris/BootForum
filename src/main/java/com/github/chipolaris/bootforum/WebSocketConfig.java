ff5efa4ebf9748d162f7dc1dad6caa9b5f427426
nguye <nguye@DESKTOP-48A15N8.fios-router.home>
2020-05-05 00:10:18 -0400

    Working on chat..

--- src/main/java/com/github/chipolaris/bootforum/WebSocketConfig.java
+++ src/main/java/com/github/chipolaris/bootforum/WebSocketConfig.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -19,5 +20,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/chat-connect").withSockJS();
+  }
+  
+  @Override
+  public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+      registration
+          .setMessageSizeLimit(5 * 1024 * 1024) // 5Mb
+          .setSendBufferSizeLimit(10 * 1024 * 1024) // 10Mb
+          .setSendTimeLimit(2 * 60 * 1000);
   }
 }
