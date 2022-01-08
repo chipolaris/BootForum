085166700c74d82832c02f99b5c996cec43bddc7
nguye <nguye@host.docker.internal>
2022-01-08 18:19:35 -0500

    Add locale for timeAgo component

---
+++ src/main/webapp/resources/js/app/timeAgoLocale.js
@@ -0,0 +1,118 @@
+  PrimeFacesExt.locales.TimeAgo['en'] = {
+    prefixAgo: null,
+    prefixFromNow: null,
+    suffixAgo: "ago",
+    suffixFromNow: "from now",
+    seconds: "less than a minute",
+    minute: "about a minute",
+    minutes: "%d minutes",
+    hour: "about an hour",
+    hours: "about %d hours",
+    day: "a day",
+    days: "%d days",
+    month: "about a month",
+    months: "%d months",
+    year: "about a year",
+    years: "%d years",
+    wordSeparator: " ",
+    numbers: []
+  }
+   
+  PrimeFacesExt.locales.TimeAgo['vi'] = {
+    prefixAgo: 'cách đây',
+    prefixFromNow: null,
+    suffixAgo: "trước",
+    suffixFromNow: "kể từ bây giờ",
+    seconds: "chưa đến một phút",
+    minute: "khoảng một phút",
+    minutes: "%d phút",
+    hour: "khoảng một tiếng",
+    hours: "khoảng %d tiếng",
+    day: "một ngày",
+    days: "%d ngày",
+    month: "khoảng một tháng",
+    months: "%d tháng",
+    year: "khoảng một năm",
+    years: "%d năm",
+    wordSeparator: " ",
+    numbers: []
+  }
+
+  PrimeFacesExt.locales.TimeAgo['ja'] = {
+    prefixAgo: "",
+    prefixFromNow: "今から",
+    suffixAgo: "前",
+    suffixFromNow: "後",
+    seconds: "1 分未満",
+    minute: "約 1 分",
+    minutes: "%d 分",
+    hour: "約 1 時間",
+    hours: "約 %d 時間",
+    day: "約 1 日",
+    days: "約 %d 日",
+    month: "約 1 ヶ月",
+    months: "約 %d ヶ月",
+    year: "約 1 年",
+    years: "約 %d 年",
+    wordSeparator: ""	
+  }
+
+  PrimeFacesExt.locales.TimeAgo['nl'] = {
+    prefixAgo: null,
+    prefixFromNow: "over",
+    suffixAgo: "geleden",
+    suffixFromNow: null,
+    seconds: "minder dan een minuut",
+    minute: "ongeveer een minuut",
+    minutes: "%d minuten",
+    hour: "ongeveer een uur",
+    hours: "ongeveer %d uur",
+    day: "een dag",
+    days: "%d dagen",
+    month: "ongeveer een maand",
+    months: "%d maanden",
+    year: "ongeveer een jaar",
+    years: "%d jaar",
+    wordSeparator: " ",
+    numbers: []
+  }
+  
+  PrimeFacesExt.locales.TimeAgo['zh-cn'] = {
+    prefixAgo: null,
+    prefixFromNow: null,
+    suffixAgo: "之前",
+    suffixFromNow: "之后",
+    seconds: "不到1分钟",
+    minute: "大约1分钟",
+    minutes: "%d分钟",
+    hour: "大约1小时",
+    hours: "大约%d小时",
+    day: "1天",
+    days: "%d天",
+    month: "大约1个月",
+    months: "%d月",
+    year: "大约1年",
+    years: "%d年",
+    numbers: [],
+    wordSeparator: ""
+  }
+  
+  PrimeFacesExt.locales.TimeAgo['zh-tw'] = {
+    prefixAgo: null,
+    prefixFromNow: null,
+    suffixAgo: "之前",
+    suffixFromNow: "之後",
+    seconds: "不到1分鐘",
+    minute: "大約1分鐘",
+    minutes: "%d分鐘",
+    hour: "大約1小時",
+    hours: "%d小時",
+    day: "大約1天",
+    days: "%d天",
+    month: "大約1個月",
+    months: "%d個月",
+    year: "大約1年",
+    years: "%d年",
+    numbers: [],
+    wordSeparator: ""
+  }
