24ed5298a170eaf288de1005ce74e180e08f2e94
nguye <nguye@host.docker.internal>
2022-01-14 20:26:11 -0500

    Add cropping of avatar upload

--- src/main/java/com/github/chipolaris/bootforum/jsf/bean/ViewDiscussion.java
+++ src/main/java/com/github/chipolaris/bootforum/jsf/bean/ViewDiscussion.java
@@ -1,5 +1,7 @@
 package com.github.chipolaris.bootforum.jsf.bean;
 
+import java.text.MessageFormat;
+
 import javax.annotation.Resource;
 
 import org.springframework.context.ApplicationEventPublisher;
@@ -9,7 +11,10 @@ import org.springframework.stereotype.Component;
 import com.github.chipolaris.bootforum.domain.Comment;
 import com.github.chipolaris.bootforum.domain.Discussion;
 import com.github.chipolaris.bootforum.event.DiscussionViewEvent;
+import com.github.chipolaris.bootforum.jsf.util.JSFUtils;
+import com.github.chipolaris.bootforum.service.AckCodeType;
 import com.github.chipolaris.bootforum.service.GenericService;
+import com.github.chipolaris.bootforum.service.ServiceResponse;
 
 @Component(value="viewDiscussion")
 @Scope("view")
@@ -72,5 +77,18 @@ public class ViewDiscussion {
 	}
 	public void setSelectedComment(Comment selectedComment) {
 		this.selectedComment = selectedComment;
+	}
+	
+	
+	public void saveDiscussionTitle() {
+		
+		ServiceResponse<Discussion> serviceResponse = genericService.updateEntity(this.discussion);
+		
+		if(serviceResponse.getAckCode() == AckCodeType.SUCCESS) {
+			JSFUtils.addInfoStringMessage(null, JSFUtils.getMessageBundle().getString("discussion.title.saved"));
+		}
+		else {
+			JSFUtils.addErrorStringMessage(null, JSFUtils.getMessageBundle().getString("unable.to.save.discussion.title"));
+		}
 	}
 }
