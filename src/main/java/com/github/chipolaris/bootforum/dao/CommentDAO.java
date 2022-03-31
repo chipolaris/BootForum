000231d565cbdb9e5df705b6ffdc99919ea391c7
Ngan DInh <ngan.dk@gmail.com>
2022-03-31 14:57:06 -0400

    change SQL syntax to work on all DBs

--- src/main/java/com/github/chipolaris/bootforum/dao/CommentDAO.java
+++ src/main/java/com/github/chipolaris/bootforum/dao/CommentDAO.java
@@ -92,12 +92,11 @@ public class CommentDAO {
 	 * This method is useful for determining if the comment is the first in the discussion
 	 */
 	public Boolean isFirstComment(Comment comment) {
+
+		String nativeQuery = "SELECT MIN(C.ID) FROM COMMENT_T C WHERE C.DISCUSSION_ID = ?1";
 		
-		String nativeQuery = "SELECT NOT EXISTS (SELECT 1 FROM COMMENT_T C WHERE C.DISCUSSION_ID = ?1 AND C.ID < ?2)";
+		Query query = entityManager.createNativeQuery(nativeQuery).setParameter(1, comment.getDiscussion().getId());
 		
-		Query query = entityManager.createNativeQuery(nativeQuery).setParameter(1, comment.getDiscussion().getId())
-				.setParameter(2, comment.getId());
-		
-		return (Boolean) query.getSingleResult();
+		return ((Number)query.getSingleResult()).longValue() == comment.getId();
 	}
 }
